04baea1bfac0d03001a7f0e80351c003072974f3
Donghyun Oh <dhgus44@naver.com>
2023-02-23 20:17:58 +0900

    #10 - 테스트 데이터를 data.sql로 추가
    이걸로 테스트 통과함

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into article (title, content, hashtag, created_by, modified_by, created_at, modified_at)
+values ('테스트', '테스트','Crimson', 'Corbin', 'Nicol', '2021-12-22 01:12:12', '2021-12-22 02:12:12');
+
