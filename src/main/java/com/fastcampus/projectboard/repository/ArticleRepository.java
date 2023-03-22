a74578cf2e848fbf030edc8fb9c844bfbd0b5cd6
Donghyun Oh <dhgus44@naver.com>
2023-03-22 16:14:22 +0900

    변경파일

--- src/main/java/com/fastcampus/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/fastcampus/projectboard/repository/ArticleRepository.java
@@ -3,7 +3,7 @@ package com.fastcampus.projectboard.repository;
 import com.fastcampus.projectboard.domain.Article;
 import com.fastcampus.projectboard.domain.QArticle;
 import com.fastcampus.projectboard.repository.querydsl.ArticleRepositoryCustom;
-import com.querydsl.core.types.dsl.DateExpression;
+import com.querydsl.core.types.dsl.SimpleExpression;
 import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -20,23 +20,22 @@ public interface ArticleRepository extends
         QuerydslPredicateExecutor<Article>,
         QuerydslBinderCustomizer<QArticle>{
 
+
     Page<Article> findByTitleContaining(String title, Pageable pageable);
     Page<Article> findByContentContaining(String content, Pageable pageable);
-    Page<Article> findByUserAccount_UserIdContaining(String UserId, Pageable pageable);
-    Page<Article> findByUserAccount_NicknameContaining(String NickName, Pageable pageable);
-    //Page<Article> findByHashtag(String hashtag, Pageable pageable);
-    //Page<Article> findByHashtags(String hashtag, Pageable pageable);
+    Page<Article> findByUserAccount_UserIdContaining(String userId, Pageable pageable);
+    Page<Article> findByUserAccount_NicknameContaining(String nickname, Pageable pageable);
+
 
     void deleteByIdAndUserAccount_UserId(Long articleId, String userid);
-
     @Override
     default void customize(QuerydslBindings bindings, QArticle root) {
         bindings.excludeUnlistedProperties(true);
-        bindings.including(root.title, root.content ,root.hashtags, root.createdAt, root.createdBy);
+        bindings.including(root.title, root.content, root.createdAt, root.createdBy);
         bindings.bind(root.title).first(StringExpression::containsIgnoreCase); // like '%${v}%'
-        bindings.bind(root.content).first(StringExpression::containsIgnoreCase); // like '%${v}%''
-        //bindings.bind(root.hashtags).first(StringExpression::containsIgnoreCase);
-        //bindings.bind(root.createdAt).first(DateExpression::eq); // like '%${v}%'
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase); // like '%${v}%'
+        //bindings.bind(root.hashtags).first(StringExpression::containsIgnoreCase); // like '%${v}%'
+        bindings.bind(root.createdAt).first((SimpleExpression::eq)); // like '%${v}%'
         bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase); // like '%${v}%'
     }
 
